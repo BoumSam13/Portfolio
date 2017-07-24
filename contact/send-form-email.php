853cbfec7893a9721e2d37998e2bc05f00760120
Samantha Boumeester <sam_boumeester41@hotmail.com>
2017-07-24 18:27:57 -0500

    Portfolio Case Studies

---
+++ contact/send-form-email.php
@@ -0,0 +1,19 @@
+<?php
+echo 'hi';
+//    $name = $_POST['name'];
+//    $email = $_POST['email'];
+//    $message = $_POST['message'];
+//    $from = 'From: TangledDemo'; 
+//    $to = 'sam.jboum@gmail.com'; 
+//    $subject = 'Hello';
+//    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+//
+//    if ($_POST['submit']) {
+//         echo 'hi';
+//        if (mail ($to, $subject, $body, $from)) { 
+//            echo '<p>Your message has been sent!</p>';
+//        } else { 
+//            echo '<p>Something went wrong, go back and try again!</p>'; 
+//        }
+//    }
+?>
